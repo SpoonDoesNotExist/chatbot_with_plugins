4c01e0c9588d397668fc015529654e043fdf6050
eduard <ed.khusnutdinow@yandex.ru>
2023-06-19 01:05:18 +0700

    Replaced `telebot` with `aiogram`

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import telebot
+from aiogram import executor, Bot, Dispatcher, types
 import openai
 
 import os
 
+import const
 from chatbot import ChatBot
 from logger import Logger
 
@@ -10,46 +11,40 @@ TG_BOT_KEY = os.environ['TG_BOT_KEY']
 OPENAI_KEY = os.environ['OPENAI_KEY']
 
 openai.api_key = OPENAI_KEY
-bot = telebot.TeleBot(TG_BOT_KEY)
+
+bot = Bot(token=TG_BOT_KEY)
+dp = Dispatcher(bot)
 
 logger = Logger()
-cb = ChatBot(
-    logger=logger
-)
+cb = ChatBot(logger=logger)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(
-        message,
-        cb.answer(message.from_user.id, 'Привет. Кто ты такой?'),
-    )
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.answer(const.BOT_HELLO_MESSAGE)
 
 
-@bot.message_handler(commands=['generate'])
-def generate(message):
+@dp.message_handler(commands=['generate'])
+async def generate(message: types.Message):
     text = message.text.replace('/generate', '').strip()
-    image_data = cb.generate(message.from_user.id, text)
+    image_data = await cb.generate(message.from_user.id, text)
+
     if image_data is None:
-        bot.reply_to(
-            message,
-            'Не получилось сгенерировать изображение.',
-        )
-    bot.send_photo(
+        await message.answer('Не получилось сгенерировать изображение.')
+    await bot.send_photo(
         message.chat.id,
         photo=image_data,
     )
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(
-        message,
-        cb.answer(message.from_user.id, message.text),
+@dp.message_handler()
+async def echo_all(message: types.Message):
+    await message.answer(
+        await cb.answer(message.from_user.id, message.text),
     )
 
 
 if __name__ == '__main__':
     logger.debug('Starting bot...')
-    bot.polling()
+    executor.start_polling(dp, skip_updates=True)
     logger.debug('Bot closed.')
