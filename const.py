caa37e5eef0f6393792dec21c1418285677c4333
eduard <ed.khusnutdinow@yandex.ru>
2023-06-19 00:20:41 +0700

    Added constants

--- const.py
+++ const.py
@@ -1,4 +1,9 @@
 BOT_NAME = 'TSU_AI_BOT'
 BOT_INSTRUCTIONS = f"""Тебя зовут {BOT_NAME}, ты дружелюбный бот, который любит поговорить на разные темы, ты отвечаешь на последнее сообщение 'user', кроме случаев, когда тебя не попросят вспомнить что было сказано."""
-API_BOT_NAME = 'text-davinci-003'
 
+API_BOT_NAME = 'text-davinci-003'
+TEMPERATURE = 0.94
+TOP_P = 0.9
+MAX_TOKENS = 1024
+
+SEPARATOR_TOKEN = '<|endoftext|>'
